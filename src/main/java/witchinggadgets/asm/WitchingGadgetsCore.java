4b00ab65454946c58a5c4fc7b88c5308158eca8c
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-02-16 23:06:19 +0100

    remove unused event registration

--- src/main/java/witchinggadgets/asm/WitchingGadgetsCore.java
+++ src/main/java/witchinggadgets/asm/WitchingGadgetsCore.java
@@ -1,17 +1,8 @@
 package witchinggadgets.asm;
 
-import witchinggadgets.WitchingGadgets;
-
-import com.google.common.eventbus.EventBus;
-import com.google.common.eventbus.Subscribe;
-
 import cpw.mods.fml.common.DummyModContainer;
-import cpw.mods.fml.common.LoadController;
-import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.ModMetadata;
-import cpw.mods.fml.common.event.FMLConstructionEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import witchinggadgets.WitchingGadgets;
 
 public class WitchingGadgetsCore extends DummyModContainer {
 
@@ -24,19 +15,4 @@ public class WitchingGadgetsCore extends DummyModContainer {
         metadata.authorList.add("BluSunrize");
         metadata.parent = "WitchingGadgets";
     }
-
-    @Override
-    public boolean registerBus(EventBus bus, LoadController controller) {
-        bus.register(this);
-        return true;
-    }
-
-    @Subscribe
-    public void modConstruction(FMLConstructionEvent event) {}
-
-    @EventHandler
-    public void preinit(FMLPreInitializationEvent event) {}
-
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {}
 }
